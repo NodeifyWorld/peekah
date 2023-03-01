4828d59cfe53b1b4fae88e35f26973ddc95ceb30
ChaozsNodeify <phil@nodeify.world>
2023-03-01 22:57:09 +1030

    edit configs

--- packages/nouns-contracts/contracts/interfaces/INounsToken.sol
+++ packages/nouns-contracts/contracts/interfaces/INounsToken.sol
@@ -20,7 +20,7 @@ pragma solidity ^0.8.6;
 import { IERC721 } from '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 
 interface INounsToken is IERC721 {
-    event NounCreated(uint256 indexed tokenId, INounsSeeder.Seed seed);
+    event NounCreated(uint256 indexed tokenId);
 
     event NounBurned(uint256 indexed tokenId);
 
@@ -28,13 +28,13 @@ interface INounsToken is IERC721 {
 
     event MinterLocked();
 
-    event DescriptorUpdated(INounsDescriptorMinimal descriptor);
+    // event DescriptorUpdated(INounsDescriptorMinimal descriptor);
 
     event DescriptorLocked();
 
-    event SeederUpdated(INounsSeeder seeder);
+    // event SeederUpdated(INounsSeeder seeder);
 
-    event SeederLocked();
+    // event SeederLocked();
 
     function mint() external returns (uint256);
 
@@ -46,11 +46,11 @@ interface INounsToken is IERC721 {
 
     function lockMinter() external;
 
-    function setDescriptor(INounsDescriptorMinimal descriptor) external;
+    // function setDescriptor(INounsDescriptorMinimal descriptor) external;
 
-    function lockDescriptor() external;
+    // function lockDescriptor() external;
 
-    function setSeeder(INounsSeeder seeder) external;
+    // function setSeeder(INounsSeeder seeder) external;
 
-    function lockSeeder() external;
+    // function lockSeeder() external;
 }
